1c0e13a8299eeb908cda0acb847ef5eef00b7ff6
iconafB <bongumenzin@iconaf.com>
2026-03-17 16:21:27 +0200

    load balancer service file

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 #import routes
 #setup cors
 #implement routes
-
+from routers.credits import credits_router
+from routers.auth import auth_router
+from routers.pings import pings_router
+app=FastAPI(title="Clients pings api")
+app.include_router(credits_router)
+app.include_router(auth_router)
+app.include_router(pings_router)
--- routers/pings.py
+++ routers/pings.py
@@ -1,5 +1,24 @@
+from fastapi import APIRouter,Depends,status
 
-
+pings_router=APIRouter(tags=["Pings Route"],prefix="/pings")
 #load pings
+@pings_router.post("")
+async def load_file_pings():
+    return
+@pings_router.post("")
+async def load_pings_payload():
+    return
+
 #get pings
+@pings_router.get("")
+async def get_pings_loaded():
+    return True
+
 #get pings status
+@pings_router.get("/status")
+async def check_pings_status():
+    async def check_pings_status():
+        return
+    
+
+
