935c76bf8b2cc5417e29e6cd35672b4e0ee8b932
iconafB <bongumenzin@iconaf.com>
2026-03-23 16:56:18 +0200

    service csv class service commit

--- config/database.py
+++ config/database.py
@@ -8,18 +8,9 @@ from settings.settings import get_settings
 class Base(DeclarativeBase):
     pass
 
-database_url=URL.create(
-    drivername="postgresql+asyncpg",
-    username=get_settings().TEST_DB_USERNAME,
-    password=get_settings().TEST_DB_PASSWORD,
-    host=get_settings().TEST_DB_HOST_NAME,
-    port=get_settings().TEST_DB_PORT,
-    database=get_settings().TEST_DB_NAME
-)
-
+database_url=URL.create(drivername="postgresql+asyncpg",username=get_settings().TEST_DB_USERNAME,password=get_settings().TEST_DB_PASSWORD,host=get_settings().TEST_DB_HOST_NAME,port=get_settings().TEST_DB_PORT,database=get_settings().TEST_DB_NAME)
 engine=create_async_engine(database_url,echo=False,future=True,pool_timeout=30,pool_recycle=1800,pool_size=10,max_overflow=20,pool_pre_ping=True)
 async_session_maker=async_sessionmaker(bind=engine,expire_on_commit=False,class_=AsyncSession)
-
 async def get_async_session()->AsyncGenerator[AsyncSession,None]:
     async with async_session_maker() as session:
         try:
--- services/csv_services.py
+++ services/csv_services.py
@@ -1,6 +1,7 @@
 from fastapi import UploadFile
 from utils.logging.logger import define_logger
 csv_logger=define_logger("csv_logger","logs/csv_route.log")
+
 class CSVClass:
     #csv data extraction
     async def data_extraction(self,data:UploadFile):
@@ -9,6 +10,7 @@ class CSVClass:
         except Exception as e:
             csv_logger.exception(f"an exception occurred while extracting data from a csv file:{str(e)}")
             raise 
+    
     #csv data deduplication
     async def deduplication(self,data:list[str])->list[str]:
         try:
@@ -16,6 +18,7 @@ class CSVClass:
         except Exception as e:
             csv_logger.exception(f"an exception occurred while running data deduplication:{str(e)}")
             raise 
+    
     async def data_bulk_insertion(self,data:list[str]):
         try:
             return
